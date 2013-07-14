89105ee6cf790090768785e30c6657a64c563e3b
aguipluto <aguipluto@gmail.com>
2013-07-14 14:36:27 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/signup', to: 'users#new'
 
@@ -62,5 +66,5 @@ SampleApp::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,17 @@ describe "User Pages" do
       it "should create a user" do
         expect {click_button submit}.to change(User,:count).by(1)
       end
+      describe "after creating a user" do
+        before {click_button submit}
+        let (:user) {User.find_by_email('user@example.com')}
+        it {should have_selector 'title', text: user.name}
+        it {should have_selector('div.alert.alert-success', text:'Welcome')}
+        it {should have_link('Sign out')}
+        describe "make a signout" do
+          before {click_link 'Sign out'}
+          it {should have_link 'Sign in'}
+        end
+      end
     end
   end
 
