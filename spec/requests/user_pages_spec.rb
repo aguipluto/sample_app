e8d96e07013530b000d05d9ab32265b4acab3264
aguipluto <aguipluto@gmail.com>
2013-07-15 10:16:06 +0200

    prueba push

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,10 @@ describe "User Pages" do
   subject { page }
   describe "signup page" do
     before { visit signup_path }
-    it {should have_selector('h1', text:'Sign up')}
-    it {should have_selector('title', text: full_title('Sign up'))}
+    it { should have_selector('h1', text :'Sign up') }
+    it { should have_selector('title', text : full_title('Sign up')) }
 
-    let(:submit) {"Create my acount"}
+    let(:submit) { "Create my acount" }
 
     describe "with invalid info" do
       it "should not create a user" do
@@ -17,23 +17,23 @@ describe "User Pages" do
 
     describe "with valid info " do
       before do
-        fill_in "Name", with: "Example user"
-        fill_in "Email", with: "user@example.com"
-        fill_in  "Password", with: "27101988"
-        fill_in "Confirmation", with: "27101988"
+        fill_in "Name", with : "Example user"
+        fill_in "Email", with : "user@example.com"
+        fill_in "Password", with : "27101988"
+        fill_in "Confirmation", with : "27101988"
       end
       it "should create a user" do
-        expect {click_button submit}.to change(User,:count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
       end
       describe "after creating a user" do
-        before {click_button submit}
-        let (:user) {User.find_by_email('user@example.com')}
-        it {should have_selector 'title', text: user.name}
-        it {should have_selector('div.alert.alert-success', text:'Welcome')}
-        it {should have_link('Sign out')}
+        before { click_button submit }
+        let (:user) { User.find_by_email('user@example.com') }
+        it { should have_selector 'title', text : user.name }
+        it { should have_selector('div.alert.alert-success', text :'Welcome') }
+        it { should have_link('Sign out') }
         describe "make a signout" do
-          before {click_link 'Sign out'}
-          it {should have_link 'Sign in'}
+          before { click_link 'Sign out' }
+          it { should have_link 'Sign in' }
         end
       end
     end
@@ -41,8 +41,24 @@ describe "User Pages" do
 
   describe "profile page " do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user)}
-    it {should have_selector('h1', text: user.name)}
-    it {should have_selector('title', text: user.name)}
+    before { visit user_path(user) }
+    it { should have_selector('h1', text : user.name) }
+    it { should have_selector('title', text : user.name) }
+  end
+
+  describe "edit " do
+    let (:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user)}
+
+    describe "page" do
+      it {should have_selector('h1', text: 'Update your profile')}
+      it {should have_selector('title', text: 'Edit user')}
+      it {should have_link('change', href: 'http://gravatar.com/emails')}
+    end
+
+    describe 'with invalid information' do
+      before {click_button 'Save changes'}
+      it {should have_content('error')}
+    end
   end
 end
